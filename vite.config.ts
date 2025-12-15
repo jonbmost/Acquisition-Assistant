96be5e8d8e4c853beffb880d8fdf8c67e31c2916
jonbmost <32501090+jonbmost@users.noreply.github.com>
2025-12-15 10:33:10 -0500

    Add Slide Ranger workspace and API

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +10,7 @@ export default defineConfig({
     sourcemap: true,
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js']
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    alias: {}
   }
 });
