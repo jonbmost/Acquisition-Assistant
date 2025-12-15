7884244b0862231fdb17e6d7d52a81ee8e61eb03
jonbmost <32501090+jonbmost@users.noreply.github.com>
2025-12-15 12:29:07 -0500

    Add Regs & Policy URL query workspace

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import SopCreationPage from './SopCreationPage';
 import StakeholderAnalysisPage from './StakeholderAnalysisPage';
 import AuthorityAssessmentPage from './AuthorityAssessmentPage';
 import SlideRangerPage from './SlideRangerPage';
+import RegsPolicyPage from './RegsPolicyPage';
 import type { KnowledgeDocument } from './types';
 import { MAX_DOCUMENT_LENGTH } from './constants';
 import { loadRepositoryKnowledgeBase } from './knowledgeBaseLoader';
@@ -150,6 +151,12 @@ const App: React.FC = () => {
   if (route.startsWith('/slide-ranger')) {
     return (
       <SlideRangerPage currentRoute={route} onNavigate={handleNavigate} />
+    );
+  }
+
+  if (route.startsWith('/url-query')) {
+    return (
+      <RegsPolicyPage currentRoute={route} onNavigate={handleNavigate} />
     );
   }
 
